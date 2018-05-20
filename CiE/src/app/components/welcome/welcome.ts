394bb19b9d91dcc48c40705671ee8c1e81445bf5
lnmssd <35195627+lnmssd@users.noreply.github.com>
2018-05-20 14:48:03 +0200

    TASK: added login page TASK: added welcome page TASK: added new navbar TASK: added new components TASK: added router link                                                                               TASK: added

---
+++ CiE/src/app/components/welcome/welcome.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.html',
+  styleUrls: ['./welcome.css']
+})
+export class Welcome implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
