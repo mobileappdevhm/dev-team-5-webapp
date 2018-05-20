92a18823cfb4861d0266461970828593c280ff8d
lnmssd <35195627+lnmssd@users.noreply.github.com>
2018-05-20 14:50:30 +0200

    TASK: added login page TASK: added welcome page TASK: added new navbar TASK: added new components TASK: added router link                                                                               TASK: added

--- CiE/src/app/components/locations/locations.component.spec.ts
+++ CiE/src/app/components/locations/locations.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginHelpComponent } from './login-help.component';
+import { LocationsComponent } from './locations.component';
 
-describe('LoginHelpComponent', () => {
-  let component: LoginHelpComponent;
-  let fixture: ComponentFixture<LoginHelpComponent>;
+describe('LocationsComponent', () => {
+  let component: LocationsComponent;
+  let fixture: ComponentFixture<LocationsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginHelpComponent ]
+      declarations: [ LocationsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginHelpComponent);
+    fixture = TestBed.createComponent(LocationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
