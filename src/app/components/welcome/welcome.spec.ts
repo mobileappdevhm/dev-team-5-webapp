21aff38a09b8dd1b7d6a7e5afff1b402d93f610c
Thai <32999078+VoThai@users.noreply.github.com>
2018-05-24 16:08:45 +0200

    Update welcome.spec.ts

--- src/app/components/welcome/welcome.spec.ts
+++ src/app/components/welcome/welcome.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Welcome } from './welcome';
 import {AppComponent} from "../../app.component";
+import { AppModule } from '../../app.module';
 
 describe('Welcome', () => {
   let component: Welcome;
@@ -23,10 +23,11 @@ describe('Welcome', () => {
   // it('should create', () => {
   //   expect(component).toBeTruthy();
   // });
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to CiE');
-  // }));
+  
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(Welcome);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to CiE');
+  }));
 });
