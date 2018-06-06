08005246a8222d7de930d81b8c5338acefca5dd3
Demonleonardo <37689787+Demonleonardo@users.noreply.github.com>
2018-06-06 19:40:42 +0200

    Update schedule.component.ts

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Favorites } from "../favorites-list/favorites";
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { LoginService } from '../../services/login/login.service';
+import { SERVER_URL } from "../../app.constants";
+import { CourseObject } from "../course-list/course";
+import { Schedule } from "./schedule";
+import {
+ getSchedules
+} from 'date-now';
 
 @Component({
   selector: 'app-shedule',
@@ -6,10 +16,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+  today = Date.now();
+  cool = false;
+  readonly SERVER_URL ='http://10.179.33.4:3000';
 
-  constructor() { }
+  favorites: Observable<Schedule[]>
+  scheduleList: Schedule[] = [];
+  courseList: CourseObject[] = [];
+  
+  username:any = '';
 
+  constructor(private http: HttpClient, private loginService: LoginService) {
+    this.username = this.loginService.username;
+  }
+
+  getSchedules() {
+    this.scheduleList = [];
+
+    this.http.get(SERVER_URL + '/Course').subscribe(data => {
+
+      for (let key in data) {
+        if(data.hasOwnProperty(key)) {
+          this.scheduleList.push(data[key]);
+        }
+      };
+    });
+  }
+
+
+  
   ngOnInit() {
+    this.getSchedules();
+  }
+
+  ngOnChange(){
+    this.today;
   }
 
 }
