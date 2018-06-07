b5b7d88ff43917d20553ddf2a106acc82e5c619b
lnmssd <35195627+lnmssd@users.noreply.github.com>
2018-06-07 16:55:55 +0200

    task: added profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginService } from '../../services/login/login.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  username:any;
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+    this.username = this.loginService.username;
   }
 
 }
