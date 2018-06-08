095e77f9a78c7dc8733ed8119f5c3d333aebc779
Thai <32999078+VoThai@users.noreply.github.com>
2018-06-08 16:14:01 +0200

    Update for Coverall

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,9 +16,14 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 90,
+        lines: 90,
+        branches: 90,
+        functions: 90
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
