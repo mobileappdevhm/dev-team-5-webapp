7f9cee94fa1ff450d82b48609c53e0713e948c3d
lnmssd <35195627+lnmssd@users.noreply.github.com>
2018-07-05 20:45:55 +0200

    FIX: bug fixed in auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,21 +1,41 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot /* , Router */} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-// import { LoginService } from '../services/login/login.service';
+import { LoginService } from '../services/login/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor( /* private router: Router,  private loginService: LoginService*/ ) { }
+  returnValue: boolean;
+
+  constructor( private router: Router,  private loginService: LoginService ) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    this.loginService.isUserLoggedIn().subscribe(e => {
 
-    // not logged in so redirect to login page with the return url
-    // this.router.navigate(['/login']);
-    // return false;
+      this.returnValue = !!e;
+      if (e) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
+
+    });
+
+    this.loginService.isUserLoggedIn().subscribe((e) => {
+      this.returnValue = !!e;
+      if (e) {
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false;
+      }
+    });
+
+    return this.returnValue;
   }
 }
